d7aa4239c3ac4aa95e8cae66fe811093b05c3bab
David Cano <davecano.244@gmail.com>
2024-04-22 17:05:12 -0400

    Placeholder Screens

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTabNavigation from './src/navigation/bottomTabNavigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App()
+{
+  return(
+    <NavigationContainer>
+      <BottomTabNavigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/navigation/bottomTabNavigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";   
+import MapScreen from '../screens/map.js';
+import SchoolingScreen from '../screens/schooling.js';
+import HousingScreen from '../screens/housing.js';
+
+
+
+//creating the bottom navigation bar object
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigation() {
+    return ( 
+
+        //creating the three tabs with the functions of our application
+        <Tab.Navigator>
+            <Tab.Screen name= "Map" component = {MapScreen} />
+            <Tab.Screen name="Schooling" component = {SchoolingScreen}/>
+            <Tab.Screen name="Housing" component = {HousingScreen}/>
+        </Tab.Navigator>
+    );
+}
+
+
