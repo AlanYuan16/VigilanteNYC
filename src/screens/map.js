68e6155929a6870ab127015011f8dd39a7743ad7
alanyuan16 <alanyuan69@gmail.com>
2024-04-24 15:48:22 -0400

    Fix bug

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function PlaceholderScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map</Text>
+    <View style={mapFormat.container}>
+      <MapView style={mapFormat.map} />
     </View>
+
   );
 }
 
@@ -21,4 +23,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+});
+
+const mapFormat = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
 });
