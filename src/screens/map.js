d26fca8c3692545f55752d7e798162bf76b00477
alanyuan16 <alanyuan69@gmail.com>
2024-04-24 21:41:47 -0400

    Added the map comp

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 import { View, Text, StyleSheet } from 'react-native';
+
+
+mapStyle= 
+[
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
 
 export default function PlaceholderScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map</Text>
+    <View style={mapFormat.container}>
+      <MapView 
+      style={mapFormat.map}
+      customMapStyle={mapStyle}
+      initialRegion = {{
+        latitude: 40.769, 
+        longitude: -73.982, 
+        latitudeDelta: 0.09,
+        longitudeDelta: 0.04,
+      }}
+      />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +62,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+});
+
+const mapFormat = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: "100%",
   },
 });
