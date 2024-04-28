e52df732134cc229920a99c1f90cb01f11c40852
Alan Yuan <100052449+AlanYuan16@users.noreply.github.com>
2024-04-28 16:53:00 -0400

    welcome page with progress bar

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import * as Progress from 'react-native-progress';
 
 const LoadingScreen = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(prevProgress => {
+        if (prevProgress < 1) {
+          return prevProgress + 0.1; 
+        } else {
+          clearInterval(interval);
+          return 1;
+        }
+      });
+    }, 300); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <ImageBackground
       source={require('../screens/homepage.jpg')}
       style={styles.background}
     >
-      <Text style={styles.title}>Welcome!</Text>  
       <Text style={styles.title}>Vigilante NYC</Text>
+      <Text style={styles.welcome}>Welcome!</Text>
+      <View style={styles.progressContainer}>
+        <Progress.Bar
+          progress={progress}
+          width={200}
+          color="white" 
+          borderColor="black" 
+          marginTop={455}
+          style={styles.progressBar}
+        />
+      </View>
       <View style={styles.container}></View>
     </ImageBackground>
   );
@@ -25,12 +53,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  progressContainer: {
+    alignItems: 'center',
+  },
   title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 70, // Adjust the marginTop as needed
+  },
+  welcome: {
     color: 'white',
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 50, // Adjust the marginTop as needed
+    position: 'absolute',
+    bottom: 200, 
+    left: 0,
+    right: 0,
   },
 });
 
