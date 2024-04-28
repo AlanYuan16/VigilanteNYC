f1fa51aef17157ea2bba7e368e9a74ff15451f58
JC9999999 <robotic32100@gmail.com>
2024-04-27 23:26:54 -0400

    added icons to bottom tab navigator

--- src/navigation/bottomTabNavigation.js
+++ src/navigation/bottomTabNavigation.js
@@ -1,14 +1,59 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";   
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
 import MapScreen from '../screens/map.js';
 import SchoolingScreen from '../screens/schooling.js';
 import HousingScreen from '../screens/housing.js';
-import Test from '../screens/componentTest.js';
+import EmojiFeedbackPage from '../screens/EmojiFeedback.js';
+
+// Image imports
+import MapIcon from '../assets/location-map-2956.png';
+import SchoolIcon from '../assets/school-building-12427.png';
+import HousingIcon from '../assets/bungalow-1881.png';
+import ReviewIcon from '../assets/discord-8249.png';
+
+// Creating the bottom navigation bar object
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigation() {
+    return (
+        // Creating the three tabs with the functions of our application
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconSource;
+
+                    if (route.name === 'Map') {
+                        iconSource = focused ? MapIcon : MapIcon;
+                    } else if (route.name === 'Schooling') {
+                        iconSource = focused ? SchoolIcon : SchoolIcon;
+                    } else if (route.name === 'Housing') {
+                        iconSource = focused ? HousingIcon : HousingIcon;
+                    } else if (route.name === 'Review') {
+                        iconSource = focused ? ReviewIcon : ReviewIcon;
+                    }
+
+                    // Return an Image component with the appropriate icon source
+                    return <Image source={iconSource} style={{ width: size, height: size }} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Schooling" component={SchoolingScreen} />
+            <Tab.Screen name="Housing" component={HousingScreen} />
+            <Tab.Screen name="Review" component={EmojiFeedbackPage} />
+        </Tab.Navigator>
+    );
+}
 
 
 
 //creating the bottom navigation bar object
-const Tab = createBottomTabNavigator();
+/*const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigation() {
     return ( 
@@ -18,9 +63,8 @@ export default function BottomTabNavigation() {
             <Tab.Screen name= "Map" component = {MapScreen} />
             <Tab.Screen name="Schooling" component = {SchoolingScreen}/>
             <Tab.Screen name="Housing" component = {HousingScreen}/>
-            <Tab.Screen name="Test" component = {Test}/>
+            <Tab.Screen name="Review" component = {EmojiFeedback}/>
         </Tab.Navigator>
     );
 }
-
-
+*/
