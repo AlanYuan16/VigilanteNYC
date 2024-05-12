07f9cfd27fc51d45c7d7ff76cd32b6f727843f0f
David Cano <davecano.244@gmail.com>
2024-05-12 15:33:56 -0400

    Removed unecessary files

--- src/hooks/FirstTimeUser.js
+++
@@ -1,3 +0,0 @@
-//this hook will be used to check if this is the first time the user has ran the app
-
-import { useEffect, useState } from 'react';
--- src/hooks/zipCode.js
+++
@@ -1,1 +0,0 @@
-//this hook will be used to set and change zip code of users location
